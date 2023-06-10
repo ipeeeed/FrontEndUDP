98c42134556dc999a24fd3f7168f222fffee04d9
Pedro Matos <85591621+ipeeeed@users.noreply.github.com>
2023-06-10 11:05:28 -0400

    teste jquery aula front-end

---
+++ ProjetoPlaceHolder/js/build-table.js
@@ -0,0 +1,23 @@
+// https://www.valentinog.com/blog/html-table/
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
+  
+  function generateTable(table, data) {
+    for (let element of data) {
+      let row = table.insertRow();
+      for (key in element) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+  }
